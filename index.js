e5b1a1330cff2fa0e0d509cd78bd7c35afe771e3
ZacharyKennedyBYU <153124492+ZacharyKennedyBYU@users.noreply.github.com>
2024-12-03 11:17:06 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const knex = require("knex")({
     connection: {
         host: process.env.RDS_HOSTNAME || "localhost",
         user: process.env.RDS_USERNAME || "postgres",
-        password: process.env.RDS_PASSWORD || "",
-        database: process.env.RDS_DB_NAME || "dbdb", // Updated database name for the intex
+        password: process.env.RDS_PASSWORD || "password",
+        database: process.env.RDS_DB_NAME || "turtleshelter", // Updated database name for the intex
         port: process.env.RDS_PORT || 5432,
         ssl: process.env.DB_SSL ? { rejectUnauthorized: false } : false,
     },
